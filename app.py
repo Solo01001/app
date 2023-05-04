280c8672e91f55632c9c19023565db2d09ffabaf
Solo <Solovyov90@gmail.com>
2023-05-04 09:17:55 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Mock data for blog posts
+posts = [
+    {
+        'title': 'First post',
+        'content': 'This is the content of the first post.'
+    },
+    {
+        'title': 'Second post',
+        'content': 'This is the content of the second post.'
+    }
+]
+
+# Route to display blog posts
+@app.route('/')
+def index():
+    return render_template('index.html', posts=posts)
+
